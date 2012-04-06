ceb2f078e41172cf7fb3fbb73ec1dcb88bfea318
Bianca Porfirio <bianca_porfirio@yahoo.com.br>
2012-04-06 19:55:37 -0300

    Criacao da classe de teste e cliente

---
+++ src/SistemaCliente.Tests/ClienteTeste.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Mteja.DI;
+using NUnit.Framework;
+
+namespace Mteja.Tests
+{
+    [TestFixture]
+    public class ClienteTeste
+    {
+        [Test]
+        public void Posso_Ter_Um_Cliente()
+        {
+            var cliente = new Cliente();
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Todo_Cliente_Tem_Um_Nome()
+        {
+            //Arrange
+            var cliente = new Cliente();
+
+            //Act
+            cliente.Nome = "Fabio Nascimento";
+
+            //Asset
+            Assert.AreEqual("Fabio Nascimento", cliente.Nome);
+        }
+
+        [Test]
+        public void Nome_Do_Cliente_Deve_Ser_Preenchido()
+        {
+            var cliente = new Cliente();
+
+            cliente.Nome = "Fabio Nascimento";
+            cliente.VerificarSeNomeEhVazio();
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Nome_Do_Cliente_Nao_Pode_Ter_Espacos_Em_Branco()
+        {
+            var cliente = new Cliente();
+
+            cliente.Nome = "   ";
+            Assert.Throws<Exception>(() => cliente.VerificarSeNomeEhVazio(), "Nome do Cliente é obrigatório.");
+        }
+
+        [Test]
+        public void Nome_Do_Cliente_Nao_Pode_Ser_Nulo()
+        {
+            var cliente = new Cliente();
+
+            cliente.Nome = null;
+
+            Assert.Throws<Exception>(() => cliente.VerificarSeNomeEhVazio(), "Nome do Cliente é obrigatório.");
+        }
+
+        [Test]
+        public void Nome_Do_Cliente_Nao_Pode_Ser_Vazio()
+        {
+            var cliente = new Cliente();
+
+            cliente.Nome = "";
+
+            Assert.Throws<Exception>(() => cliente.VerificarSeNomeEhVazio(), "Nome do Cliente é obrigatório.");
+        }
+
+        [Test]
+        public void Todo_Cliente_Tem_Uma_Data_Cadastro()
+        {
+            var cliente = new Cliente();
+
+            var dataAgora = DateTime.Now;
+
+            cliente.DataCadastro = dataAgora;
+
+            Assert.AreEqual(dataAgora, cliente.DataCadastro);
+        }
+
+        [Test]
+        public void Data_De_Cadastro_Do_Cliente_Nao_Pode_Ser_Nulo()
+        {
+            var cliente = new Cliente();
+
+            Assert.Throws<Exception>(() => cliente.VerificarSeDataEhNula(), "Data de Cadastro do cliente é obrigatório.");
+        }
+    }
+}
